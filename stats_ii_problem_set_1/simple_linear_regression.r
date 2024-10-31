e5b5c9e5fcaefdb665ad5af963416169cf07b907
Ruth Grace Wong <ruthgracewong@gmail.com>
2024-10-30 18:27:08 -0700

    correct agecat for problem set 1

--- stats_ii_problem_set_1/simple_linear_regression.r
+++ stats_ii_problem_set_1/simple_linear_regression.r
@@ -13,9 +13,10 @@ ce_data <- read_dta("data/ce621.dta")
 ce_data$log10totchg <- log10(ce_data$totchg)
 
 # assign age categories - TODO check if there are supposed to be two or three
-ce_data$agecat <- ifelse(ce_data$age <= 50, "<=50", ifelse(ce_data$age >= 65, ">=65", "<=64"))
+ce_data$agecat <- ifelse(ce_data$age <= 60, "<=60", ">60")
 
 # group by age category and sex
 ce_grouped <- ce_data %>% group_by(agecat, sex)
 
-# check how many there are - looks like 2, age 42 and 49: ce_data %>% filter(age < 50)
+ce_data %>%
+    unite(as_factor(sex), agecat, sep = "_")
